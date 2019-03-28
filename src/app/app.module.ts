deb1a3aa706a41f6384dd8ff3b6a224a56f6723a
Aleksa Doderovic <aleksa.doderovic@itsystems.me>
2019-03-28 10:44:53 +0100

    make component for listing phone numbers

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { PhoneItemsComponent } from './phone-items/phone-items.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PhoneItemsComponent
   ],
   imports: [
     BrowserModule
