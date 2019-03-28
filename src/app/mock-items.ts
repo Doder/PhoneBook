2065d97a6402692a0092181c0b425468ea2a6494
unknown <aleksadoderovic@gmail.com>
2019-03-28 18:56:58 +0100

    create class for phone items and add styles

--- src/app/mock-items.ts
+++ src/app/mock-items.ts
@@ -1,9 +1,34 @@
+import {PhoneItem} from './phone-item';
 
-export const ITEMS = [
-  '123095182905',
-  '1234',
-  '6664367346',
-  '12346451234',
-  '4441235213542',
-  '4454221323412',
+export const ITEMS : PhoneItem[] = [
+  {
+    "id": "c111e970-50b5-11e9-881d-b5c11200862b",
+    "name": "Jimmy",
+    "email": "jimmy@gmail.com",
+    "number": "393-0224622",
+    "tags": [
+    "Work",
+    "Paris"
+    ]
+    },
+    {
+    "id": "19ad2b80-50b6-11e9-890c-35926e3ac265",
+    "name": "Mark",
+    "email": "mark@bmail.com",
+    "number": "340-8496466",
+    "tags": [
+    "Store",
+    "Friend"
+    ]
+    },
+    {
+    "id": "28722530-50b6-11e9-b3b9-3f2a6948afb9",
+    "name": "Nicolas",
+    "email": "nico@hotmail.com",
+    "number": "333-7432463",
+    "tags": [
+    "Neighbour",
+    "daycare"
+    ]
+    }
 ];
---
+++ src/app/phone-item.ts
@@ -0,0 +1,7 @@
+export class PhoneItem{
+    id: string;
+    name: string;
+    email: string;
+    number: string;
+    tags: string[];
+}
